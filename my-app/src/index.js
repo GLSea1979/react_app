cf3f46b0248f7174efc5ccbb4dc19e18383fa114
GLSea1979 <builtbygl@gmail.com>
2018-04-11 20:59:00 -0700

    building

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+// import './index.css';
+// import App from './App';
+import 'font-awesome/css/font-awesom.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Timeline from './components/Timeline/Timeline';
+import Container from './Container';
+import Header from './Header';
+
+export const load = () => {
+  console.log('hi');
+  ReactDom.render(<Timeline />, document.getElementById('demo1'));
+  ReactDom.render(<Container />, document.getElementById('demo2'));
+  ReactDom.render(<Header />, document.getElementById('headerDemo'));
+
+};
+load();
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
